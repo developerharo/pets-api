ffe2be37663bd0d642f4bdd35fcb871a781c6541
José Haro <developerharo@gmail.com>
2021-05-21 17:21:32 -0500

    Changed cluster database to remote MongoDB Atlas

--- app/config/config.js
+++ app/config/config.js
@@ -1,4 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
-    DB: process.env.DB || 'mongodb://localhost:27017/pets-api',
+    DB:
+        process.env.DB ||
+        'mongodb+srv://haro:haro@novena-gen.0ttr9.mongodb.net/pets?retryWrites=true&w=majority',
 };
