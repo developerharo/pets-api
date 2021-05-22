ab2c82a603d47fd479089efdcdcce349e8bcecec
José Haro <developerharo@gmail.com>
2021-05-21 20:21:29 -0500

    Issues about connection string

--- app/config/config.js
+++ app/config/config.js
@@ -2,6 +2,6 @@ module.exports = {
     HOST: process.env.HOST || '0.0.0.0',
     PORT: process.env.PORT || 3000,
     DB:
-        process.env.DB ||
+        process.env.DB_URL ||
         'mongodb+srv://haro:haro@novena-gen.0ttr9.mongodb.net/pets?retryWrites=true&w=majority',
 };
--- app/config/database.js
+++ app/config/database.js
@@ -1,3 +1,6 @@
+// Importar variables de entorno locales
+require('dotenv').config({ path: 'variables.env' });
+
 const mongoose = require('mongoose');
 const CONFIG = require('./config');
 mongoose.set('useNewUrlParser', true);
@@ -9,12 +12,15 @@ module.exports = {
     connection: null,
     connect: function () {
         if (this.connection) return this.connection;
-        return mongoose
-            .connect(CONFIG.DB)
-            .then((connection) => {
-                this.connection = this.connection;
-                console.log('Connected to Database');
-            })
-            .catch((error) => console.log(error));
+        return (
+            mongoose
+                // .connect(process.env.DB_URL)
+                .connect(CONFIG.DB)
+                .then((connection) => {
+                    this.connection = this.connection;
+                    console.log('Connected to Database');
+                })
+                .catch((error) => console.log(error))
+        );
     },
 };
